09d5c7e2d0b1956f437b9cb582b15bd08f8758e1
Stefany <efyguevara@gmail.com>
2020-01-14 11:19:21 -0300

    elimino importación de componentes bootstrap y el comentario con los datos de la ciudad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup, Button } from 'react-bootstrap';
 import Weather from './Weather';
 
 function App() {
@@ -18,13 +17,3 @@ function App() {
 }
 
 export default App;
-
-
-  // {
-  //   "id": 3646738,
-  //   "name": "Caracas",
-  //   "country": "VE",
-  //   "coord": {
-  //     "lon": -66.879189,
-  //     "lat": 10.48801
-  //   }
