f2629b4b923018ad66ea25c37d9ffc293c1dd113
Stefany <efyguevara@gmail.com>
2020-01-15 10:08:47 -0300

    Tips para mostrar de acuerdo al rango de temperatura especificado en Tips.js

---
+++ src/dataTips.js
@@ -0,0 +1,12 @@
+const tips = {"Frio": ["Recuerda usar guantes y bufanda para evitar resfriados.", 
+                      "Toma bebidas caliente para mantener el calor corporal.",
+                      "¡Colocate bálsamo labial y mucha crema! Cuando baja la temperatura la piel se reseca con facilidad."],
+              "Normal": ["La temperatura esta ideal para ir de paseo, ¡Disfruta al aire libre con un rico picnic!.", 
+                        "No olvides cuidar tu piel, usa bloquador solar.",
+                        "Mantente hidratado, beber agua ayuda a mantener la salud de la piel y a no sentirnos agotados por el calor."],
+              "Calor": ["No olvides Usar protector solar.",
+                        "Bebe al menos 8 vasos de agua al día para mantenerte hidratado.", 
+                        "Usar una sombrilla, te ayudara a reducir los rayos UV y te mantendrá fresco."]
+              };
+
+export default tips; 
