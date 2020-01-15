f6c4e4aa4cf09c3642636df428f7a6625a99e5d1
Stefany <efyguevara@gmail.com>
2020-01-14 21:08:02 -0300

    - Se cambia el protocolo de http a https para que lea la información del endpoint desde el deploy.
    - Agrego la funciones:
    	getData() para optimizar la llamada a la data.
    
    	firstLetterMay(string) se crea para dar formato a la información de la data, coloca en mayúscula la primera letra de una oración.
    
    	backgroundTemp(temp) para hacer cambio del color de fondo dependiendo del rango de temperatura.

--- src/Weather.js
+++ src/Weather.js
@@ -1,61 +1,104 @@
 import React, { useState, useEffect } from 'react';
+import './weather.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 
 function Weather() {
-  const [ data, setData ] = useState({});
+  const [data, setData] = useState({});
   const caracasCityId = 3646738;
   const apiKey = '884bca85e08368d78a24f4affffa40a0';
-  
-    async function fetchData() {
-      const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${caracasCityId}&units=metric&appid=${apiKey}`)       
-        resp.json()
-        .then((res) => {
-          console.log(res)
-          setData(res)
-        })
-    }
-    useEffect(() => {
+
+  async function fetchData() {
+    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${caracasCityId}&units=metric&lang=es&appid=${apiKey}`)
+    resp.json()
+      .then((res) => {
+        backgroundTemp(res.main.temp);
+        setData(res)
+      })
+  }
+
+  useEffect(() => {
     fetchData();
-}, [])
- 
+  }, [])
+
+  const getData = (data, attr, indicator) => {
+    return data ? Math.round(data[attr]).toString() + indicator : 'No Disponible';
+  };
+
+  function firstLetterMay(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function backgroundTemp(temp) {
+    const bodyBg = document.getElementById('boddy');
+
+    if (temp <= 10) {
+      bodyBg.classList.add("cold");
+    }
+    if (temp >= 11 && temp <= 20) {
+      bodyBg.classList.add("normal");
+    }
+    if (temp >= 21 && temp <= 30) {
+      bodyBg.classList.add("hot");
+    }
+  }
 
   return (
     <>
-      <Container>
+      <Container className="weather-style">
         <Row>
           <Col>
-            <h1>{data.name}</h1>
-          </Col>
-          
-          <Col>
-            <h1>{ data.main ? Math.round(data.main.temp) : null }°</h1>
-            <p>{ data.weather ? data.weather[0].description : null }</p>
-
+            <h1>{data ? data.name : 'No disponible'}</h1>
           </Col>
         </Row>
-        <div>
-          <p>Sensación Térmica: { data.main ? Math.round(data.main.feels_like) : null }°</p>
-          <Row>
-            <Col>
-              <p>Mín: { data.main ? Math.round(data.main.temp_min) : null }°</p>
-            </Col>
-            <Col>
-              <p>Máx: { data.main ? Math.round(data.main.temp_max) : null }°</p>
-            </Col>
-          </Row>
-          <p>Velocidad el viento: { data.wind ? data.wind.speed : null }km/h</p>
-          {/* <p>Visibilidad: { data.visibility ? data.visibility : null }km</p> */}
-          <p>Nubosidad: { data.clouds ? data.clouds.all : null }%</p>
 
-          <p>Humedad: { data.main ? data.main.humidity : null }%</p>
-        </div>
+        <Row>
+          <Col>
+            <h1>{getData(data.main, 'temp', '°')}</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>{data.weather ? firstLetterMay(data.weather[0].description) : 'No disponible'}</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <p>Sensación Térmica: {getData(data.main, 'feels_like', '°')}</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <p>Mín: {getData(data.main, 'temp_min', '°')} / Máx: {getData(data.main, 'temp_max', '°')}</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <p>Viento: {getData(data.wind, 'speed', 'km/h')}</p>
+          </Col>
+
+          <Col sm>
+            <p>Visibilidad: {data.visibility ? data.visibility / 1000 + 'km' : 'No Disponible'}</p>
+          </Col>
+
+          <Col sm>
+            <p>Nubosidad: {getData(data.clouds, 'all', '%')}</p>
+          </Col>
+
+          <Col sm>
+            <p>Humedad: {getData(data.main, 'humidity', '%')}</p>
+          </Col>
+        </Row>
       </Container>
     </>
   );
 }
 
 export default Weather;
+
 
   // *****Información de la ciudad*****
   // {
